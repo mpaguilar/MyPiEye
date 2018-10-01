429c111b23629c22bef10a47f5c99bebcbbb8de3
Michael Aguilar <bitflip@slowlink.net>
2018-09-30 20:33:31 -0500

    Consolidate cmdline options

--- PiEye/main.py
+++ PiEye/main.py
@@ -26,7 +26,7 @@ global_defaults = {
     'logfile': None,
     'loglevel': 'DEBUG',
     'color': True,
-    'config': 'pyeye.ini'
+    'config': 'pieye.ini'
 }
 
 defaults = windows_defaults
@@ -36,7 +36,8 @@ if platform.system() == 'Linux':
 defaults.update(global_defaults)
 
 
-@click.group()
+
+@click.command()
 @click.option('--loglevel', default='CRITICAL',
               type=click.Choice(CLI.LOG_LEVELS),
               help='python log levels')
@@ -44,8 +45,26 @@ defaults.update(global_defaults)
 @click.option('--color/--no-color', default=defaults['color'], help='Pretty color output')
 @click.option('--config',
               default=defaults['config'], help='key/val (.ini) config file', callback=CLI.load_config)
-@click.pass_context
-def motion(ctx, loglevel, logfile, color, config):
+@click.option('--workdir', default=defaults['workdir'], help='Temporary directory for processing images')
+@click.option('--savedir', default=defaults['savedir'], help='Where to store files')
+@click.option('--gdrive', default=defaults['gdrive'], help='Google drive folder')
+@click.option('--camera', default=defaults['camera'], help='Camera to watch')
+@click.option('--resolution', default=defaults['resolution'], type=click.Choice(['small', '720p', '1080p']),
+              help='Camera resolution')
+@click.option('--show_timings/--no_show_timings', default=defaults['show_timings'],
+              help='Used for debugging/optimizing')
+def motion(loglevel,
+          logfile,
+          color,
+          config,
+          workdir,
+          savedir,
+          gdrive,
+          url,
+          camera,
+          resolution,
+          show_timings):
+    """Start capturing and watching"""
     pass
 
 
