cc8f0ba03114b8b9ea574ac939cd7b02541d4ff3
Michael Aguilar <bitflip@slowlink.net>
2018-10-22 17:08:00 -0500

    New opencv on RPi script

--- fast_opencv.sh
+++ fast_opencv.sh
@@ -1,33 +1,19 @@
-sudo apt-get install -y build-essential cmake pkg-config
-sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
-sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install -y libxvidcore-dev libx264-dev
-sudo apt-get install -y libgtk2.0-dev
-sudo apt-get install -y libatlas-base-dev gfortran
-sudo apt-get install -y python2.7-dev python3-dev
+sudo apt install -y git
+sudo apt install -y libopencv-dev
 
-mkdir opencv_work
-cd opencv_work
+sudo apt install -y python3-pip
 
-wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.1.0.zip
-unzip opencv.zip
-wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.1.0.zip
-unzip opencv_contrib.zip
+sudo apt install -y liblapack3
+sudo apt install -y libopenblas-base
+sudo apt install -y libatlas3-base
+sudo apt install -y libatlas-base-dev
 
+sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev 
+sudo apt install -y libjasper-dev
 
-cd opencv-3.1.0/
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D INSTALL_PYTHON_EXAMPLES=ON \
-    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.1.0/modules \
-    -D BUILD_EXAMPLES=OFF \
-    -D ENABLE_PRECOMPILED_HEADERS=OFF \
-    -D WITH_FFMPEG=ON \
-    ..
+sudo apt install -y gstreamer1.0-tools
+sudo apt install -y libgtk-3-dev
 
-make
+sudo pip3 install -U setuptools
+sudo pip3 install opencv-python-headless
 
-sudo make install
-sudo ldconfig
