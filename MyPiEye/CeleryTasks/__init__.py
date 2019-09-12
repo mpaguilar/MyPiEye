863662b4c6a848a707b8e2227503ff02169b6720
Michael Aguilar <bitflip@slowlink.net>
2019-09-11 21:21:32 -0500

    Captures are logged to db

--- MyPiEye/CeleryTasks/__init__.py
+++ MyPiEye/CeleryTasks/__init__.py
@@ -1,21 +1,22 @@
 from functools import partial
 from datetime import datetime
 
-from io import BytesIO
-
-import numpy as np
 import pickle
+import json
+import pytz
 
 from celery import Celery
 from celery.utils.log import get_task_logger
 
+import redis
+
 from MyPiEye.Storage.minio_storage import MinioStorage
-from MyPiEye.CLI import get_config_value, enable_log, set_loglevel
+from MyPiEye.CLI import get_config_value
 
 celery_app = Celery('imghandler')
 celery_app.config_from_object({
     'task_serializer': 'pickle',
-    'accept_content' : ['json', 'pickle']
+    'accept_content': ['json', 'pickle']
 })
 log = get_task_logger(__name__)
 
@@ -24,23 +25,51 @@ app_config = {}
 
 
 @celery_app.task()
-def accept_capture(img_bytes, dt_stamp: datetime, cam_id: str):
+def store_capture(img_bytes, dt_stamp: datetime, cam_id: str):
     # bout-time/cam0/20190907/23/15/2019.09.07.23.15.49.220779.jpg
 
-    lvl = get_config_value(app_config, 'global', 'loglevel', 'LOG_LEVEL')
-    fmt = get_config_value(app_config, 'global', 'log_format', 'LOG_FORMAT')
-    enable_log(fmt=fmt)
-    set_loglevel(lvl)
-
-    log.info('Accepting capture')
+    log.info('Storing capture to minio')
 
     cv2img = pickle.loads(img_bytes)
 
     mio = MinioStorage(app_config)
     mio.upload(cv2img, dt_stamp, cam_id)
 
-    log.info('Uploaded')
+    log.info('Stored {} capture to minio'.format(dt_stamp.isoformat()))
 
+
+@celery_app.task()
+def register_capture(dt_stamp: datetime, cam_id: str):
+
+    log.warning('Adding capture to redis db')
+    rcfg = partial(get_config_value, app_config, 'db_redis')
+
+    redis_host = rcfg('host', 'DB_REDIS_HOST')
+    redis_port = rcfg('port', 'DB_REDIS_PORT')
+    redis_db = rcfg('db', 'DB_REDIS_DB')
+
+    redis_password = rcfg('password', 'DB_REDIS_PASSWORD')
+    timezone = get_config_value(app_config, 'global', 'timezone', 'TIMEZONE', 'US/Central')
+
+    redis_prefix = rcfg('db_prefix', 'DB_REDIS_PREFIX')
+
+    rsrv = redis.Redis(redis_host, redis_port, redis_db, redis_password)
+    fmt = '%Y%m%d/%H/%M/%Y.%m.%d.%H.%M.%S.%f'
+    key = '{}/{}/{}'.format(redis_prefix, cam_id, dt_stamp.strftime(fmt))
+
+    tz = pytz.timezone(timezone)
+    local_time = tz.localize(dt_stamp)
+
+    val = {
+        'utc_time': dt_stamp.isoformat(timespec='microseconds'),
+        'local_time': local_time.isoformat(timespec='microseconds'),
+        'camera_id': cam_id
+    }
+
+    val = json.dumps(val)
+
+    ret = rsrv.set(key, val)
+    log.warn('Redis updated with {}'.format(dt_stamp.isoformat()))
 
 
 @celery_app.task()
